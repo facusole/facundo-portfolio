a097b71d2fdfd1cf3048ead5db8ee10843abfdc5
facusole <facundojsole@gmail.com>
2025-03-03 11:48:48 -0300

    refactor navbar to live inside layout, add navbar links, update package json

---
+++ src/data/navbar_links.ts
@@ -0,0 +1,29 @@
+const NAVBAR_LINKS = [
+	{
+		name: "Home",
+		href: "/",
+		active: true,
+	},
+	{
+		name: "About Me",
+		href: "/about-me",
+		active: false,
+	},
+	{
+		name: "Projects",
+		href: "/projects",
+		active: false,
+	},
+	{
+		name: "Github",
+		href: "https://github.com/facusole",
+		active: false,
+	},
+	{
+		name: "Linkedin",
+		href: "https://www.linkedin.com/in/facundo-sole/",
+		active: false,
+	},
+];
+
+export default NAVBAR_LINKS;
