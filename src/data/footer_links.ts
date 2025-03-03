f6aa35f51e0648a381143339f3383092bc11dc2b
facusole <facundojsole@gmail.com>
2025-03-03 16:06:44 -0300

    add new footer link file and fix footer links design

---
+++ src/data/footer_links.ts
@@ -0,0 +1,18 @@
+export const FOOTER_LINKS = [
+	{
+		name: "About Me",
+		href: "/about-me",
+	},
+	{
+		name: "Github",
+		href: "https://github.com/facusole",
+	},
+	{
+		name: "Linkedin",
+		href: "https://www.linkedin.com/in/facundo-sole/",
+	},
+    {
+		name: "Projects",
+		href: "/projects",
+	},
+];
