7b4099bdfd2ec911efbc02c691ca39e6c7ed405d
Facundo <facu@androbin.co>
2025-09-06 22:58:27 -0300

    fix vite aliases config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ export default defineConfig({
   integrations: [react()],
   vite: {
     resolve: {
-      alias: [
-        { find: '@components', replacement: '/src/components' },
-        { find: '@pages', replacement: '/src/pages' },
-        { find: '@layouts', replacement: '/src/layouts' },
-        { find: '@data', replacement: '/src/data' }
-      ]
+      alias: {
+        '@components': new URL('./src/components', import.meta.url).pathname,
+        '@pages': new URL('./src/pages', import.meta.url).pathname,
+        '@layouts': new URL('./src/layouts', import.meta.url).pathname,
+        '@data': new URL('./src/data', import.meta.url).pathname
+      }
     }
   }
 });
