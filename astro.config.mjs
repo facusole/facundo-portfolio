5fa40ca6b92402f5984be6d1e4427bfe4a98c526
Facundo <facu@androbin.co>
2025-09-06 22:55:32 -0300

    remove vite aliases config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,14 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    resolve: {
+      alias: [
+        { find: '@components', replacement: '/src/components' },
+        { find: '@pages', replacement: '/src/pages' },
+        { find: '@layouts', replacement: '/src/layouts' },
+        { find: '@data', replacement: '/src/data' }
+      ]
+    }
+  }
 });
