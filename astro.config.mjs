f3f0e937c91c7e0ea4dd72da2c717c485cf8f7ea
Facundo <facu@androbin.co>
2025-09-06 23:01:45 -0300

    remove all aliases imports to fix vercel build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  vite: {
-    resolve: {
-      alias: {
-        '@components': new URL('./src/components', import.meta.url).pathname,
-        '@pages': new URL('./src/pages', import.meta.url).pathname,
-        '@layouts': new URL('./src/layouts', import.meta.url).pathname,
-        '@data': new URL('./src/data', import.meta.url).pathname
-      }
-    }
-  }
 });
